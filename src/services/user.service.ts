982220422d7bf805e5264e9e43186945b01c08ff
Durbek <saydaliyevdurbek0512@gmail.com>
2024-12-31 14:53:37 +0500

    fixes user create response

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -14,32 +14,29 @@ export class UserService {
     this.axios = axiosService.getAxiosInstance();
   }
 
-  async startUser(message: TelegramBot.Message) {
+  async startUser(message: any) {
     const user = {
       id: message.from.id.toString(),
       chatId: message.from.id.toString(),
       firstName: message.from.first_name,
       lastName: message.from.last_name || '',
       username: message.from.username || null,
-      isPremium: false,
+      isPremium: message.from.is_premium || false,
       languageCode: 'eng',
     };
-   
 
-    const foundUser = await this.findUserByChatId(message.from.id.toString());
-    if (foundUser) {
-      return foundUser;
-    } else if (foundUser === null) {
+    const response = await this.findUserByChatId(message.from.id.toString());
+    if (response.user !== null) {
+      return response.user;
+    } else if (response.user === null) {
       try {
         const response = await this.axios.post(
           `${this.configService.get('BACKEND_URL')}/api/user?app_key=${this.configService.get('APP_KEY')}`,
           user,
         );
         return response.data;
-      } catch (error) {
-      }
+      } catch (error) {}
     }
-
   }
 
   private async findUserByChatId(chatId: string) {
